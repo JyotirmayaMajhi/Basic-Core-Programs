33349006f092673bfc35f3f2b2e3a21f6e4bb739
unknown <jyotirmayamajhi1998@gmail.com>
2021-11-02 19:01:57 +0530

    Added Quotient and Remainder

---
+++ src/com/bridgelabz/corejava/QuotientRemainder.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.corejava;
+
+public class QuotientRemainder {
+
+	public static void main(String[] args) {
+		int dividend=85;
+		int divisor =3;
+		
+		int Quotient= dividend/divisor;
+		int Remainder = dividend % divisor;
+		
+		System.out.println("The Quotient is: " +Quotient);
+		System.out.println("The Remainder is: " +Remainder);
+	}
+
+}
